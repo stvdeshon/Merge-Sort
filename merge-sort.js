1f4e08e9427f362ad8b7959744412ec80ed42523
stvdeshon <93623464+stvdeshon@users.noreply.github.com>
2022-10-26 12:17:59 -0600

    Add files via upload

---
+++ merge-sort.js
@@ -0,0 +1,32 @@
+function merge(left, right) {
+    let arr = []
+    // Break out of loop if any one of the array gets empty
+    while (left.length && right.length) {
+        // Pick the smaller among the smallest element of left and right sub arrays 
+        if (left[0] < right[0]) {
+            arr.push(left.shift())  
+        } else {
+            arr.push(right.shift()) 
+        }
+    }
+    
+    return [ ...arr, ...left, ...right ]
+}
+
+function mergeSort(array) {
+    const half = array.length / 2
+    
+    // Base case or terminating case
+    if(array.length < 2){
+      return array 
+    }
+    
+    const left = array.splice(0, half)
+    return merge(mergeSort(left),mergeSort(array))
+  }
+
+
+const arrayOne = [8, 4, 3, 7, 6, 12, 1, 9];
+console.log(mergeSort(arrayOne));
+const arrayTwo = [9, 1, 6, 5, 20, 3, 7];
+console.log(mergeSort(arrayTwo));
